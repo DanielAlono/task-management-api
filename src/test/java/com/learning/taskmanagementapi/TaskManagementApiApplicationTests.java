8e5087b36defb8a2f20379b19e94f4ddbb33d01e
Alonso-Gonzalez Daniel-Emilio <daniel.alonso-gonzalez@inetum.com>
2025-02-04 15:02:15 +0100

    adding maven project

---
+++ src/test/java/com/learning/taskmanagementapi/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.taskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
